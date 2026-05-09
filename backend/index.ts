64fdf53f86dfff943d0fbe4e0d94323a9c2b2473
khizarshah01 <khizar.shah010@gmail.com>
2026-05-10 00:22:37 +0530

    init

---
+++ backend/index.ts
@@ -0,0 +1,59 @@
+import express from 'express';
+const { tavily } = require('@tavily/core');
+import { z } from "zod";
+const { zodToJsonSchema } = require("zod-to-json-schema");
+const app = express();
+const port = 3000;
+const client = tavily({ apiKey: process.env.TAVILY_API_KEY });
+import { GoogleGenAI } from "@google/genai";
+import { PROMPT_TEMPLATE, SYSTEM_PROMPT } from './prompt';
+app.use(express.json());
+const ai = new GoogleGenAI({
+  apiKey: process.env.GEMINI_API_KEY,
+});
+
+const ingredientSchema = z.object({
+    followup: z.array(z.string()),
+    answer: z.string(),
+});
+
+
+app.post('/conversation', async (req, res) => {
+
+    // user query
+    const userQuery = req.body.query;
+
+
+    // search the query
+    const webResponse = await client.search(userQuery, {
+        searchDepth: "advanced"
+    });
+
+    //response from web search
+    const webResults = webResponse.results;
+
+    // llm request
+    const prompt = PROMPT_TEMPLATE.replace("{{WEB_SEARCH_RESULTS}}", JSON.stringify(webResults)).replace("{{USER_QUERY}}", userQuery);
+    const response = await ai.models.generateContentStream({
+        model: "gemini-3-flash-preview",
+        contents: prompt,
+        config: {
+            systemInstruction: SYSTEM_PROMPT,
+            temperature: 0.7,
+            responseMimeType: "application/json",
+            responseSchema: zodToJsonSchema(ingredientSchema),
+        },
+    });
+
+    for await (const chunk of response) {
+        res.write(chunk.text ?? "");
+    } 
+    console.log("Web search results: ", webResults);
+    res.write("------SOURCEs------\n");
+    //
+    webResults.forEach((result: any) => res.write(JSON.stringify(result)));
+
+    res.end();
+})
+
+app.listen(port);
---
+++ backend/prompt.ts
@@ -0,0 +1,42 @@
+export const SYSTEM_PROMPT = `You are an expert assistant called Purplexity. Your job is simple, given the USER_QUERY and a bunch of web search responses, try to answer the user query to the best of your abilities. 
+YOU DONT HAVE ACCESS TO ANY TOOLS. You are being given all the context that is needed to answer the query.
+
+You also need to return follow up questions to the user based on the question they have asked.
+The response needs to be structured like this:
+
+<ANSWER>
+This is where the actual query should be answered.
+</ANSWER>
+
+<FOLLOWUP>
+<question> first follow up question  </question>
+<question> second follow up question  </question>
+<question> third follow up question  </question>
+</FOLLOWUP>
+
+EXAMPLE:
+USER_QUERY: "I want to learn Python, can you suggest me best way?"   
+
+RESPONSE:
+<ANSWER>
+To learn Python, you can start with online tutorials and courses such as Codecademy, Coursera, or Udemy. Additionally, practicing coding on platforms like LeetCode and HackerRank can help solidify your understanding. Don't forget to work on real projects to apply what you've learned!
+</ANSWER>
+
+<FOLLOWUP>
+<question> What is your current level of programming experience? </question>
+<question> Are you looking for free resources or are you open to paid courses? </question>
+<question> Do you have any specific goals in mind for learning Python (e.g., web development, data science, etc.)? </question>
+</FOLLOWUP>
+
+Make sure to answer the question as best as you can and generate follow up questions that are relevant to the user query.
+`
+
+export const PROMPT_TEMPLATE = `
+
+## Web Search Results
+{{WEB_SEARCH_RESULTS}}
+
+## USER_QUERY
+USER_QUERY: {{userQuery}}
+
+`
